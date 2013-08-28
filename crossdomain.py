246c62c4cc707a0b63ded548372a26c4491336b4
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2013-08-29 01:08:03 +0200

    note source code origin

--- crossdomain.py
+++ crossdomain.py
@@ -1,3 +1,5 @@
+# from http://flask.pocoo.org/snippets/56, public domain
+
 from datetime import timedelta
 from flask import make_response, request, current_app
 from functools import update_wrapper
