8f88bf9e62c75fd65b33d879c4d74b2176ecd9cb
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2013-08-29 01:01:48 +0200

    2nd attempt at crossdomain

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, logging, make_response
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name
 from pygments.formatters import HtmlFormatter
+from crossdomain import crossdomain
 
 import markdown
 
@@ -23,16 +24,14 @@ def index():
 
 
 @app.route('/', methods=['POST'])
+@crossdomain(origin="*")
 def pygments():
   lang = request.form["lang"]
   code = request.form["code"]
-  pyg = pygmentize(lang, code)
-  resp = make_response(pyg, 200)
-  resp.headers["Access-Control-Allow-Origin"] = "*"
-  return resp
+  return pygmentize(lang, code)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     # For debugging & automatic relaoding of flask apps, set debug=True
-    # app.debug = True
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
